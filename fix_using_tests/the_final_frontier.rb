11517f70fe714435e1c90fa3ea601775acac4d40
James Rogers <jarogers095@gmail.com>
2019-12-05 23:30:54 +0000

    Automatically backed up by Learn

--- fix_using_tests/the_final_frontier.rb
+++ fix_using_tests/the_final_frontier.rb
@@ -12,11 +12,11 @@ def crew
 end
 
 def greet_crew (crew)
-  crew.each {|crew_member| puts "Hello #{crew_member}."}
+  crew.each {|crew_member| "Hello #{crew_member}."}
 end
 
 def engage
   date = generate_star_date
-  state_log(date)
-  greet_crew(crew)
+  puts state_log(date)
+  puts greet_crew(crew)
 end
